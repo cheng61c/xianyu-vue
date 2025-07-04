cbab1140477ee740ea3040b7a7331d8471052b18
suancaixianyu <3501869534@qq.com>
2025-07-05 01:12:52 +0800

    test

--- src/hook/regex.ts
+++ src/hook/regex.ts
@@ -4,18 +4,10 @@ export const extractImageSrcs = (html: string): string[] => {
   const regex = /<img[^>]+src=["']([^"']+)["']/g
   const result: string[] = []
   let match: RegExpExecArray | null
-  const currentProtocol = window.location.protocol // 'http:' 或 'https:'
 
   while ((match = regex.exec(html)) !== null) {
     const src = match[1]
-    // 只保留协议一致的链接或相对路径
-    if (
-      src.startsWith(currentProtocol) ||
-      src.startsWith('//') ||
-      !src.startsWith('http')
-    ) {
-      result.push(formatLink(src))
-    }
+    result.push(formatLink(src))
   }
 
   return result
